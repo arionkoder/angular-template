cf120f1e1c252135cadcbc81363849f9628dbb9a
Nicolas Naso <nicolasna@arionkoder.com>
2021-11-25 10:34:13 -0300

    refactor: remove empty lifecycle method

--- src/app/features/feature-example/components/feature-example/feature-example.component.ts
+++ src/app/features/feature-example/components/feature-example/feature-example.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-feature-example',
   templateUrl: './feature-example.component.html',
-  styleUrls: ['./feature-example.component.scss']
+  styleUrls: ['./feature-example.component.scss'],
 })
-export class FeatureExampleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class FeatureExampleComponent {}
